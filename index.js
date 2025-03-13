2446a162485b3362027763715364d765ff8f52e6
Anantha Debnath <ananthadebnath103@gmail.com>
2025-03-13 21:44:39 +0600

    .env file setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 // Middleware
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri ="mongodb+srv://ananthadeb:msDRqOHUcRzgPHs4@cluster0.emv2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emv2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
