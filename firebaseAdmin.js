2285019d4a0a2f4846003df0557456262f119373
Anantha Debnath <ananthadebnath103@gmail.com>
2026-07-16 21:38:29 +0600

    firebase tocekn folded eith env

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service_Account.json"); // Replace with your actual file path
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FB_PROJECT_ID,
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 module.exports = admin;
